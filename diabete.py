6ac227f9c394ad9b551d6a79babe310ae438d518
Uddeshya Tyagi <uddeshyatyagi775@gmail.com>
2021-03-24 15:57:39 +0530

    Update diabete.py

--- diabete.py
+++ diabete.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sklearn.metrics import accuracy_score
+from sklearn import metrics
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from PIL import Image
